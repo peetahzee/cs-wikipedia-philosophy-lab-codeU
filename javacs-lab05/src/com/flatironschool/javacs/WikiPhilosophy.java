04d47acc74a934f3bc622c49a23af4cb6e3791ef
Peter Zhang <ptz@ptz-macbookpro.roam.corp.google.com>
2016-06-29 23:17:18 -0700

    Implement WikiPhilosophy

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -1,8 +1,13 @@
 package com.flatironschool.javacs;
 
 import java.io.IOException;
+import java.lang.Math;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Deque;
+import java.util.Hashtable;
 
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
@@ -11,41 +16,116 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
-	final static WikiFetcher wf = new WikiFetcher();
-	
-	/**
-	 * Tests a conjecture about Wikipedia and Philosophy.
-	 * 
-	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
-	 * 
-	 * 1. Clicking on the first non-parenthesized, non-italicized link
-     * 2. Ignoring external links, links to the current page, or red links
-     * 3. Stopping when reaching "Philosophy", a page with no links or a page
-     *    that does not exist, or when a loop occurs
-	 * 
-	 * @param args
-	 * @throws IOException
-	 */
-	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
+  final static String BASE_URL = "https://en.wikipedia.org";
+  final static String BEGIN_URL = "/wiki/Java_(programming_language)";
+  final static String END_URL = "/wiki/Philosophy";
+  final static WikiFetcher wf = new WikiFetcher();
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+  
+  public static void main(String[] args) throws IOException {
+    // Used as a Queue for a BFS, a Stack for a DFS.
+    Deque<String> urlsToVisit = new ArrayDeque<String>();
+    // Keep track of which URLs we have visited, so we don't get ourselves stuck in a loop.
+    List<String> visitedUrls = new ArrayList<String>();
+    // Keep track of how we got to each page, so that we can find our trace back to the BEGIN_URL.
+    Hashtable<String, String> referrers = new Hashtable<String, String>();
+    boolean pathFound = false;
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
+    // Begin with the BEGIN_URL
+    urlsToVisit.push(BEGIN_URL);
+
+    while(!urlsToVisit.isEmpty() && !pathFound) {
+      String currentUrl = urlsToVisit.pop();
+      visitedUrls.add(currentUrl);
+
+      Elements paragraphs = wf.fetchWikipedia(BASE_URL + currentUrl);
+      List<String> pageUrls = new ArrayList<String>();
+
+      for (Element p : paragraphs) {
+        pageUrls.addAll(getLinksFromParagraph(p));
+      }
+
+      // Reverse the order of all page urls so that when we're done pushing all the URLS
+      // to the stack, the first URL in the page will be the first URL in the current page.
+      Collections.reverse(pageUrls);
+
+      // Add all the URLs to the list of URLs to visit.
+      for (String newUrl : pageUrls) {
+        if(!visitedUrls.contains(newUrl)) {
+          urlsToVisit.push(newUrl);
+          // Record how we ended up at newUrl.
+          referrers.put(newUrl, currentUrl);
+
+          // Check if one of the links in this page is the END_URL; which means we'll be done!
+          if (newUrl.equals(END_URL)) {
+            pathFound = true;
+          }
         }
+      }
+    }
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
-	}
+    if (pathFound) {
+      System.out.println("=================");
+      System.out.println("Path found!");
+      System.out.println("=================");
+
+      // Back trace how we ended up at END_URL.
+      String backtraceUrl = END_URL;
+      System.out.println(backtraceUrl);
+      while(backtraceUrl != BEGIN_URL) {
+        String referrerUrl = referrers.get(backtraceUrl);
+        System.out.println(referrerUrl);
+        backtraceUrl = referrerUrl;
+      }
+    } else {
+      System.out.println("=================");
+      System.out.println("No path found :(");
+      System.out.println("=================");
+    }
+  }
+
+  static List<String> getLinksFromParagraph(Element paragraph) {
+    Iterable<Node> iter = new WikiNodeIterable(paragraph);
+    List<String> newUrls = new ArrayList<String>();
+    int numOfParens = 0;
+
+    for (Node node: iter) {
+      if (node instanceof Element) {
+        Element el = (Element) node;
+        if (isWikiLink(el) && !isItalicized(node) && numOfParens == 0) {
+          newUrls.add(el.attr("href"));
+        }
+      } else if (node instanceof TextNode) {
+        numOfParens += countChar(node.toString(), '(');
+        numOfParens = Math.max(numOfParens - countChar(node.toString(), ')'), 0);
+      }
+    }
+    return newUrls;
+  }
+
+  static boolean isWikiLink(Element el) {
+    return el.tagName() == "a" && el.attr("href").indexOf("/wiki/") == 0;
+  }
+
+  static boolean isItalicized(Node node) {
+    while(node.parentNode() != null) {
+      Node parent = node.parentNode();
+      if (parent instanceof Element) {
+        Element parentEl = (Element) parent;
+        if (parentEl.tagName() == "i" || parentEl.tagName() == "em") {
+          return true;
+        }
+      }
+      node = parent;
+    }
+    return false;
+  }
+
+  static int countChar(String s, char targetChar) {
+    int count = 0;
+    for (char c : s.toCharArray()) {
+      if (c == targetChar) { count++; }
+    }
+    return count;
+  }
 }
--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophyTest.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophyTest.java
@@ -6,8 +6,13 @@ package com.flatironschool.javacs;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
+import org.junit.After;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /**
  * @author downey
@@ -15,22 +20,43 @@ import org.junit.Test;
  */
 public class WikiPhilosophyTest {
 
-
-	/**
-	 * Test method for {@link com.flatironschool.javacs.WikiPhilosophy#main(java.lang.String[])}.
-	 */
 	@Test
-	public void testMain() {
-		// Because this lab is more open-ended than others, we can't provide unit
-		// tests.  Instead, we just check that you've modified WikiPhilosophy.java
-		// so it doesn't throw an exception.
-		String[] args = {};
-		try {
-			WikiPhilosophy.main(args);
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail();
-		}
+	public void testProcessParagraphStandardLink() {
+		Document doc = Jsoup.parse("<p>This is a link to a <a href=\"/wiki/test\">test</a>. " + 
+		  	"This is an <a href=\"/wiki/test2\">another link</a>.</p>");
+		Element el = doc.getElementsByTag("p").get(0);
+		List<String> newUrls = WikiPhilosophy.getLinksFromParagraph(el);
+		assertEquals(newUrls.size(), 2);
+		assertTrue(newUrls.contains("/wiki/test"));
+		assertTrue(newUrls.contains("/wiki/test2"));
 	}
 
+	@Test
+	public void testProcessParagraphExternalLink() {
+		Document doc = Jsoup.parse("<p>This is a link to a <a href=\"/wiki/test\">test</a>. " +
+			  "This is a link to <a href=\"https://google.com\">Google</a>.</p>");
+		Element el = doc.getElementsByTag("p").get(0);
+		List<String> newUrls = WikiPhilosophy.getLinksFromParagraph(el);
+		assertEquals(newUrls.size(), 1);
+		assertTrue(newUrls.contains("/wiki/test"));
+	}
+
+	@Test
+	public void testProcessParagraphItalicizedLink() {
+		Document doc = Jsoup.parse("<p>This is an <em>italicized link to a <a href=\"/wiki/test\">test</a></em>. " +
+			  "This is a <b><u><i>bold underlined italicized link to <a href=\"/wiki/test2\">Google</a></i></u></b>.</p>");
+		Element el = doc.getElementsByTag("p").get(0);
+		List<String> newUrls = WikiPhilosophy.getLinksFromParagraph(el);
+		assertEquals(newUrls.size(), 0);
+	}
+
+	@Test
+	public void testProcessParagraphParenLink() {
+		Document doc = Jsoup.parse("<p>This is a link to a <a href=\"/wiki/test\">test</a>. " + 
+		  	"This is a (by-the-way <a href=\"/wiki/test2\">another link</a>).</p>");
+		Element el = doc.getElementsByTag("p").get(0);
+		List<String> newUrls = WikiPhilosophy.getLinksFromParagraph(el);
+		assertEquals(newUrls.size(), 1);
+		assertTrue(newUrls.contains("/wiki/test"));
+	}
 }
